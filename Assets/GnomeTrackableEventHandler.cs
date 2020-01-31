a7677bc8e006c6a95ae52d6dcbfed4798a440b1f
LozMarshall <lwk.marshall@gmail.com>
2020-01-31 14:44:27 +0000

    Zone 2 update

---
+++ Assets/GnomeTrackableEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class GnomeTrackableEventHandler : DefaultTrackableEventHandler {
+
+    private Gnome myGnome;
+
+    public Text consoleLog;
+
+    //private bool isPlaying = false;
+    
+
+    protected override void Start()
+    {
+        myGnome = this.GetComponent<Gnome>();
+
+        consoleLog.text += "started gnome trackable start routine\n";
+
+        base.Start();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        
+        if (!myGnome.isPlaying)
+        {
+            myGnome.audio.Play(0);
+            myGnome.isPlaying = true;
+            consoleLog.text += "gnome set to play audio";
+        }
+    }
+    
+}
--- Assets/SamplesResources/SceneAssets/ImageTargets/Scripts/VideoTrackableEventHandler.cs
+++ Assets/SamplesResources/SceneAssets/ImageTargets/Scripts/VideoTrackableEventHandler.cs
@@ -5,16 +5,37 @@ Vuforia is a trademark of PTC Inc., registered in the United States and other
 countries.
 ===============================================================================*/
 
+using UnityEngine;
+
 public class VideoTrackableEventHandler : DefaultTrackableEventHandler
 {
     #region PROTECTED_METHODS
 
     protected override void OnTrackingLost()
     {
-        mTrackableBehaviour.GetComponentInChildren<VideoController>().Pause();
+        //mTrackableBehaviour.GetComponentInChildren<VideoController>().Pause();
 
-        base.OnTrackingLost();
+        VideoController[] videos = GetComponentsInChildren<VideoController>();
+
+        foreach (VideoController video in videos)
+        {
+            video.Pause();
+        }
+
+        //base.OnTrackingLost(); //commented out for now as we dont want to see the extended tracking.
     }
+
+	protected override void OnTrackingFound()
+	{
+
+		base.OnTrackingFound();
+		VideoController[] videos = GetComponentsInChildren<VideoController>();
+
+        foreach (VideoController video in videos)
+        {
+            video.Play();
+        }
+	}
 
     #endregion // PROTECTED_METHODS
 }
