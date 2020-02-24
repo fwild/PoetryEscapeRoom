15e947db819351327e8bd3dc2a293b4f1cb645a6
LozMarshall <lwk.marshall@gmail.com>
2020-02-24 14:02:10 +0000

    code cleanup: VideoPlayerManager.cs

--- Assets/_scripts/VideoPlayerManager.cs
+++ Assets/_scripts/VideoPlayerManager.cs
@@ -7,8 +7,6 @@ using UnityEngine.Video;
 public class VideoPlayerManager : Singleton<VideoPlayerManager>
 {
     
-
-    //public double timecode;
     public bool isPlaying = false;
 
     public GameObject videoObject;
@@ -33,27 +31,12 @@ public class VideoPlayerManager : Singleton<VideoPlayerManager>
         } else
         {
             Debug.Log("running else statement playVideo");
-            //videoObject = new GameObject();
-            //videoObject.transform.parent = target.transform;
 
             videoPlane = Instantiate(prefab, target.transform.position, target.transform.rotation);
             videoPlane.transform.parent = target.transform;
-
-
-            //videoObject.AddComponent<MeshRenderer>();
-            //videoObject.AddComponent<MeshFilter>();
-
-            //videoObject.GetComponent<MeshFilter>().mesh = CreatePlaneMesh();
-            //videoObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Default-Material");
-
-
-            //videoPlayer = videoObject.AddComponent<VideoPlayer>();
+            
             videoPlayer = videoPlane.GetComponent<VideoPlayer>();
             videoPlayer.clip = videoClip;
-            
-            //videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
-            //videoPlayer.targetMaterialRenderer = videoObject.GetComponent<MeshRenderer>();
-            //videoPlayer.targetMaterialProperty = "_MainTex";
 
             videoPlayer.Play();
             isPlaying = true;
@@ -70,31 +53,7 @@ public class VideoPlayerManager : Singleton<VideoPlayerManager>
             
         }
     }
-
-    //private Mesh CreatePlaneMesh()
-    //{
-    //    Mesh m = new Mesh();
-    //    m.name = "PlaneMesh";
-    //    m.vertices = new Vector3[] {
-    //         new Vector3( _width/2f, -_height/2f, 0 ),
-    //         new Vector3( -_width/2f, -_height/2f, 0 ),
-    //         new Vector3( -_width/2f, _height/2f, 0 ),
-    //         new Vector3( _width/2f, _height/2f, 0 )
-    //     };
-    //    m.uv = new Vector2[] {
-    //         new Vector2 (1, 0),
-    //         new Vector2 (0, 0),
-    //         new Vector2 (0, 1),
-    //         new Vector2 (1, 1)
-    //     };
-    //    m.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
-    //    m.RecalculateNormals();
-
-    //    return m;
-    //}
-
-
-
+    
     protected VideoPlayerManager()
     {
 
